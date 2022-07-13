02a42879863f598b275f2da9690195563f9f11d9
SIARHEI-SMANTSAR <siarhei.smantsar@gmail.com>
2022-07-13 20:17:50 +0200

    chore: fix SERVER_ENDPOINT

--- src/utils/get-server-path.ts
+++ src/utils/get-server-path.ts
@@ -1,4 +1,2 @@
-export const getServerPath = (path: string, { addDomain = true, addApiPrefix = true } = {}): string =>
-  `${(addDomain && process.env.REACT_APP_SERVER_ENDPOINT) || ''}${
-    (addApiPrefix && process.env.API_PREFIX) || '/api/v1'
-  }${path}`;
+export const getServerPath = (path: string): string =>
+  `${process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : ''}/api/v1${path}`;
