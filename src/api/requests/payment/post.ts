52bda52e7274c46806e93dd87b6aa7c8e63af91d
SIARHEI-SMANTSAR <siarhei.smantsar@gmail.com>
2022-09-11 17:23:18 +0200

    feat: add redirectUrl to payment

--- src/api/requests/payment/post.ts
+++ src/api/requests/payment/post.ts
@@ -2,9 +2,15 @@ import { requestsFactory } from 'redux-requests-factory';
 
 import { api, ENDPOINTS } from '../../index';
 
-const request = (): Promise<void> => api.post(ENDPOINTS.PAYMENT, {});
+const request = (): Promise<{ redirectUrl: string }> => api.post(ENDPOINTS.PAYMENT, {});
 
 export const { doRequestAction: doPaymentAction } = requestsFactory({
   request,
   stateRequestKey: 'payment',
+  fulfilledActions: [
+    ({ response: { redirectUrl } }) => {
+      window.location.href = redirectUrl;
+      return null;
+    },
+  ],
 });
