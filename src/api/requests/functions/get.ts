9c4c3110fd7fd31aef305df6711f05d09944abb2
Vlad Surahin <v.suragin@gmail.com>
2022-11-01 22:44:51 +0400

    fix: handle functions endpoint changes

--- src/api/requests/functions/get.ts
+++ src/api/requests/functions/get.ts
@@ -1,10 +1,10 @@
 import { requestsFactory } from 'redux-requests-factory';
 
-import type { Function } from '../../../types/api/function';
-import type { QueryParams } from '../../../types/api/queryParams';
-import { api, ENDPOINTS } from '../../index';
+import type { Function } from '@app/types/api/function';
+import type { QueryParams } from '@app/types/api/queryParams';
+import { api, ENDPOINTS } from '@app/api';
 
-const request = (fileId: QueryParams['fileId']): Promise<Function[]> => api.get(ENDPOINTS.FUNCTIONS(fileId));
+const request = (fileId: QueryParams['fileId']): Promise<Function['name'][]> => api.get(ENDPOINTS.FUNCTIONS(fileId));
 
 export const {
   loadDataAction: loadFunctionsAction,
@@ -13,7 +13,7 @@ export const {
   request,
   stateRequestKey: 'functions',
   serializeRequestParameters: (fileId: string) => `${fileId}`,
-  transformResponse: (response: Function[] | undefined) => response || [],
+  transformResponse: (response: Function['name'][] | undefined) => response || [],
 });
 
 export const functionsActions = { loadFunctionsAction };
