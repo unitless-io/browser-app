8f322cdc82db9fda5759f72ffeb0e328c8d8ffbd
SIARHEI-SMANTSAR <siarhei.smantsar@gmail.com>
2022-11-01 11:05:36 +0100

    feat: add GtmTrustModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import GtmTrustModal from './components/GtmTrustModal';
 import Loading from './components/Loading';
 import Router from './router';
 
 const App = () => {
-
   return (
     <>
       <Router />
       <Loading />
+      <GtmTrustModal />
     </>
   );
 };
