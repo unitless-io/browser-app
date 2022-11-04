396cabbf48c4be2e1d84d9a750693b3f56a5a47f
Vlad Surahin <v.suragin@gmail.com>
2022-11-04 17:11:25 +0000

    feat: add feedback and bug buttons

--- src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
@@ -5,8 +5,9 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-
-import RemoveAllCallsButton from '@app/components/RemoveAllCallsButton';
+import Button from '@mui/material/Button';
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import BugReportIcon from '@mui/icons-material/BugReport';
 
 const ResponsiveAppBar = () => {
   return (
@@ -20,7 +21,8 @@ const ResponsiveAppBar = () => {
           </Link>
 
           <Box sx={{ flexGrow: 1 }}></Box>
-          <RemoveAllCallsButton />
+          <Button variant="contained" startIcon={<ReviewsIcon />} color="info" href="https://forms.gle/7YtsmpjJev8opaRd6" target="_blank" sx={{ marginRight: 2 }}>Leave Feedback</Button>
+          <Button variant="contained" startIcon={<BugReportIcon />} color="warning" href="https://github.com/unitless-io/loader/issues" target="_blank">Report bug</Button>
         </Toolbar>
       </Container>
     </AppBar>
